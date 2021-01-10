54527984f52ade9824b4e4d26373dba7becadef3
colterlichti <colterlichti2012@gmail.com>
2021-01-10 01:02:01 -0500

    Added multiple load points

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import { ComponentApplication } from '/ComponentSystem/core/ComponentApplication.js';
 import { MasterVoxelController } from '/controllers/MasterVoxelController.js';
+import { Viewer3D } from '/ComponentSystem/ui/components/Viewer3D.js';
 
 // Application
 let application;
 
+let voxelController;
 
+// Initialization before full load
 document.addEventListener('DOMContentLoaded', (event) => {
     application = new ComponentApplication();
     application.initializeApp();
 
-    let voxelController = new MasterVoxelController(application);
+    voxelController = new MasterVoxelController(application);
+});
+
+// Build views once page is fully loaded and ready to display
+window.addEventListener('load', (event)=>{
+    console.log('huh');
     voxelController.createView();
 });

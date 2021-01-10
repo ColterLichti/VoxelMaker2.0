f2b8e5524e2b041bcc4662fb55a563b2d559e258
colterlichti <colterlichti2012@gmail.com>
2021-01-09 21:32:12 -0500

    Handles logic for the voxel page (main page)

---
+++ controllers/MasterVoxelController.js
@@ -0,0 +1,45 @@
+import {Controller} from '/ComponentSystem/controller/Controller.js';
+import { BUILD_VoxelCreation_page } from '/ui-builds/VoxelCRUD-page.js';
+
+export class MasterVoxelController extends Controller{
+
+    constructor(application){
+        super(application);
+    }
+
+    createView(){
+        this.view = BUILD_VoxelCreation_page();
+        this.application.UI.addPage('voxel', this.view);
+        let c = this.ctx;
+
+        // 🔊👂 Set up subsciptions here 🔊👂
+
+        // ⚡ Annon func to stop the dumbest 'feature' of js. (this context change in handlers) ⚡
+        // ⚡ The event object has a target already you fucking mongs!                          ⚡
+        c.purePageButton.subscribe('click', (event)=>{this.NavigateToPurePage_handler(event)});
+        c.partialPageButton.subscribe('click', (event)=>{this.NavigateToPartialPage_handler(event)});
+        
+        c.pureRadio.subscribe('change', (event)=>{this.TypeChanged_handler(event)});
+        c.partialRadio.subscribe('change', (event)=>{this.TypeChanged_handler(event)});
+    }
+
+    NavigateToPurePage_handler(event){
+        // Use application to navigate to page
+        console.log('Navigating...');
+    }
+
+    NavigateToPartialPage_handler(event){
+        console.log('Navigating...');
+    }
+
+    TypeChanged_handler(event){
+        let typ = this.ctx.pureRadio.groupValue;
+
+        if(typ === 'pure'){
+            this.ctx.paVoxelRail.enabled = false;
+        }
+        else if (typ === 'partial'){
+            this.ctx.paVoxelRail.enabled = true;
+        }
+    }
+}

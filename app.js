833a2117d6e86c9b1cf6be60d2f4fb1f01d046ca
colterlichti <colterlichti2012@gmail.com>
2021-01-10 20:00:42 -0500

    All views now have controllers

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import { ComponentApplication } from '/ComponentSystem/core/ComponentApplication.js';
 import { MasterVoxelController } from '/controllers/MasterVoxelController.js';
+import { PureController } from '/controllers/PureController.js';
+import { PartialController } from '/controllers/PartialController.js';
 import { Viewer3D } from '/ComponentSystem/ui/components/Viewer3D.js';
+
+import { FileManager } from '/ComponentSystem/util/FileManager.js';
 
 // Application
 let application;
 
 let voxelController;
+let pureController;
+let partialController;
 
 // Initialization before full load
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -13,10 +19,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     application.initializeApp();
 
     voxelController = new MasterVoxelController(application);
+    pureController = new PureController(application);
+    partialController = new PartialController(application);
 });
 
 // Build views once page is fully loaded and ready to display
-window.addEventListener('load', (event)=>{
-    console.log('huh');
+window.addEventListener('load', (event) => {
     voxelController.createView();
+    pureController.createView();
+    partialController.createView();
+
+    application.UI.showPage('voxel');
 });

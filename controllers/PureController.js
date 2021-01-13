782766463c329b50e78ed0a4cb85d5babd4b2ad6
colterlichti <colterlichti2012@gmail.com>
2021-01-13 01:39:43 -0500

    Implemented bulk assign and all buttons function

--- controllers/PureController.js
+++ controllers/PureController.js
@@ -6,10 +6,14 @@ export class PureController extends Controller {
 
     fileManager;
 
+    // Testing
+    pureModel;
+
     constructor(application) {
         super(application);
 
         this.fileManager = new FileManager();
+        this.pureModel = {faceFiles:[]};
     }
 
     createView() {
@@ -21,8 +25,12 @@ export class PureController extends Controller {
         c.voxelPageButton.subscribe('click', (event) => { this.NavigateToVoxelPage_handler(event) });
         c.partialPageButton.subscribe('click', (event) => { this.NavigateToPartialPage_handler(event) });
 
-        c.North.subscribe('aux-click', (event) => { this.NorthAuxClick_handler(event) });
-        c.North.subscribe('face-click', (event) => { this.NorthFaceClick_handler(event) });
+        // Subscribe all face buttons 🐵
+        for(let key in c.faceButtons){
+            let btn = c.faceButtons[key];
+            btn.subscribe('aux-click', (event) => { this.FaceClick_handler(event) });
+            btn.subscribe('face-click', (event) => { this.FaceClick_handler(event) });
+        }
     }
 
     NavigateToVoxelPage_handler(event) {
@@ -33,13 +41,57 @@ export class PureController extends Controller {
         this.application.UI.showPage('partial');
     }
 
-    NorthAuxClick_handler(event) {
+    async FaceClick_handler(event) {
+        if(event.face){
+            let files;
+            let chooseStatus;
+            try{
+                files = await this.fileManager.openFile();
+                chooseStatus = true;
+            }catch{
+                // User closed dialog or cancelled
+                chooseStatus = false;
+            }
 
-    }
-    async NorthFaceClick_handler(event) {
-        let file;
-        file = await this.fileManager.openFile();
-        event.component.faceImage = '/images/check.png';
-        event.component.toolTip = file.name;
+            let comp = event.component;
+            if(chooseStatus){
+                comp.selected = true;
+                comp.toolTip = files[0].name;
+
+                this.pureModel.faceFiles[comp.direction] = files[0];
+            }else{
+                event.component.selected = false;
+                event.component.toolTip = '';
+                this.pureModel.faceFiles[comp.direction] = '';
+            }
+        }
+        // Bulk assign this buttons value to all buttons
+        // Also works for clearing values from all buttons
+        else if (event.aux){
+            let comp = event.component;
+
+            let file = this.pureModel.faceFiles[comp.direction];
+
+            if(file !== undefined && file instanceof File){
+                for(let key in this.ctx.faceButtons){
+                    // Update model
+                    this.pureModel.faceFiles[key] = file;
+
+                    // Update buttons
+                    this.ctx.faceButtons[key].selected = true;
+                    this.ctx.faceButtons[key].toolTip = file.name;
+                }
+            }
+            else{
+                for(let key in this.ctx.faceButtons){
+                    // Update model
+                    this.pureModel.faceFiles[key] = undefined;
+
+                    // Update buttons
+                    this.ctx.faceButtons[key].selected = false;
+                    this.ctx.faceButtons[key].toolTip = '';
+                }
+            }
+        }
     }
 }

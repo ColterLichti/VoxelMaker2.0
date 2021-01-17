d8110bd5615b8a5442bdc69557826692fb2fc550
colterlichti <colterlichti2012@gmail.com>
2021-01-17 04:06:19 -0500

    Used threejs OBJLoader instead of custom one

--- ComponentSystem/util/OBJTools.js
+++ ComponentSystem/util/OBJTools.js
@@ -1,4 +1,7 @@
-import * as Three from '/node_modules/three/build/three.module.js';
+import { BufferGeometry, BufferAttribute, MeshBasicMaterial, Mesh, Vector3, Vector2, ObjectLoader } from '/node_modules/three/build/three.module.js';
+import { OBJLoader } from '/node_modules/three/examples/jsm/loaders/OBJLoader.js';
+
+
 
 export class OBJParser {
 
@@ -6,108 +9,19 @@ export class OBJParser {
 
 Object.defineProperties(OBJParser, {
     parseText: {
-        value: (text, hardEdge) => {
-            const geometry = new Three.BufferGeometry();
+        value: (text) => {
+            const loader = new OBJLoader();
+            let mod = loader.parse(text);
 
-            const vertices = [];
-            const normals = [];
-            const uvs = [];
-            const indices = [];
-
-            // Split on newline regex
-            const lines = text.split(/\r?\n/);
-
-            // Fill arrays from file
-            for (let i = 0; i < lines.length; i++) {
-                const line = lines[i].toLowerCase();
-
-
-                if (line !== undefined && line !== '') {
-                    const prefix = line.substring(0, 2).trim();
-                    const raw = line.split(' ');
-
-                    switch (prefix) {
-                        case 'o':
-                            geometry.name = lines[i].substring(2).split('_')[0].trim();
-                            break;
-
-                        case 'v':
-                            vertices.push(raw[1], raw[2], raw[3]);
-                            break;
-
-                        case 'vt':
-                            uvs.push(raw[1], raw[2]);
-                            break;
-
-                        case 'vn':
-                            normals.push(raw[1], raw[2], raw[3]);
-                            break;
-
-                        case 'f':
-                            OBJParser._parseFace(line, indices);
-                            break;
-                    }
-                }
-            }
-
-            // Transpose data to geometry
-            OBJParser._transposeData(geometry, vertices, uvs, normals, indices);
-
-            // Return the geometry
-            return geometry;
+            return mod.children[0];
         },
         writable: false,
-        enumerable: false,
-        configurable: false
-    },
-    _parseFace: {
-        value: (line, indices) => {
-            const groups = line.split(' ');
-            // Index 0 will contain the 'f' label not any data
-            for (let i = 1; i < groups.length; i++) {
-                const group = groups[i].split('/');
-
-                // Don't allow quad faces!
-                if(group.length > 4)
-                    throw 'This models faces have not been triangulated!';
-
-                indices.push({ v: group[0] - 1, t: group[1] - 1, n: group[2] - 1 });
-            }
-        },
-        writable: false,
-        enumerable: false,
-        configurable: false
-    },
-    _transposeData:{
-        value:(geometry, vertices, uvs, normals, indices) =>{
-
-            console.log(vertices);
-
-            const t_uvs = [];
-            const t_norms = [];
-            const t_inds = [];
-
-            geometry.setAttribute('position', new Three.BufferAttribute(new Float32Array(vertices), 3));
-
-            for (let i = 0; i < indices.length; i++) {
-                const index = indices[i];
-                
-                // Add the vertices index
-                t_inds.push(index.v);
-                // Align uv array with verts
-                t_uvs[index.v] = uvs[index.t];
-                // Align normals array with verts
-                t_norms[index.v] = normals[index.n];                
-            }
-
-            geometry.setAttribute('normal', new Three.BufferAttribute(new Float32Array(t_norms), 3));
-            geometry.setAttribute('uv', new Three.BufferAttribute(new Float32Array(t_uvs), 2));
-        },
-        writable: false,
-        enumerable: false,
+        enumerable: true,
         configurable: false
     }
 });
+
+
 
 export class PureDataModel {
     constructor() {
@@ -115,8 +29,22 @@ export class PureDataModel {
             pureFaces: {
                 value: [],
                 writable: false,
-                enumerable: false,
+                enumerable: true,
                 configurable: false
+            },
+            toJSON: {
+                value: () => {
+                    let scope = this;
+                    let obj = { 'type': scope.constructor.name, 'pureFaces': [] };
+
+                    for (let key in this.pureFaces) {
+                        const face = this.pureFaces[key];
+
+                        obj.pureFaces.push({ 'key': key, 'value': face, 'type': face.constructor.name });
+                    }
+
+                    return obj;
+                }
             }
         });
     }
@@ -125,36 +53,29 @@ export class PureDataModel {
 export class PureFace {
     constructor() {
         Object.defineProperties(this, {
-            geometry: {
-                value: new Three.BufferGeometry(),
+            mesh: {
+                value: new Mesh(),
                 writable: true,
-                enumerable: false,
-                configurable: false
-            },
-            material: {
-                value: new Three.MeshBasicMaterial(),
-                writable: true,
-                enumerable: false,
+                enumerable: true,
                 configurable: false
             },
             sourceFileName: {
                 value: '',
                 writable: true,
-                enumerable: false,
+                enumerable: true,
                 configurable: false
             },
             clone: {
-                value: ()=>{
+                value: () => {
                     const clone = new PureFace();
-
-                    clone.geometry = this.geometry.clone();
-                    clone.material = this.material.clone();
-                    clone.sourceFileName = this.sourceFileName;
-
+                    const mesh = new Mesh(this.mesh.geometry.clone(), this.mesh.material.clone());
+                    clone.mesh = mesh;
                     return clone;
-                }
+                },
+                writable: true,
+                enumerable: false,
+                configurable: false
             }
         });
-
     }
 }
